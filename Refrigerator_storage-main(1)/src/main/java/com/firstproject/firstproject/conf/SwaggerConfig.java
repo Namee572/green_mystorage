23e18657a8eb321cda745777ee15928d255e1b0e
박준재 <wnswo1109@naver.com>
2024-03-29 16:15:30 +0900

    240329

---
+++ Refrigerator_storage-main(1)/src/main/java/com/firstproject/firstproject/conf/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.firstproject.firstproject.conf;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info( title = "냉장고 게사판",
+                description = "냉장고 게시판",
+                version = "v1.0.0"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi(){
+        String[] paths = {"/**"};
+        return GroupedOpenApi.builder()
+                .group("냉장고 게시판") // 추후 전체 컨트롤러로 변경
+                .pathsToMatch(paths)
+                .build();
+    }
+//"/member/**", "/borad/**", "/foodinfo/**",
+//        "/main/foodstorage/**", "/api/comments/**"
+//
+}
---
+++ Refrigerator_storage-main(1)/src/main/java/com/firstproject/firstproject/recipeboard/RecipeBoard/RecipeBoardRepository.java
@@ -0,0 +1,27 @@
+package com.firstproject.firstproject.recipeboard.RecipeBoard;
+
+import com.firstproject.firstproject.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface RecipeBoardRepository extends JpaRepository<RecipeBoard, Long> {
+
+    Page<RecipeBoard> findByTitleContaining(String searchKeyword, Pageable pageable);
+    List<RecipeBoard> findAllByOrderByCreateDateDesc();
+
+    List<RecipeBoard> findAllByOrderByViewCountDesc();
+    List<RecipeBoard> findAllByMember(Member member);
+
+    @Query(value = "SELECT * FROM recipeboard r WHERE r.user_id = :user_id", nativeQuery = true)
+    List<RecipeBoard> findRecipeBoardsByUserId(@Param("user_id") Long userId);
+
+    RecipeBoard findBytitle(String title);
+}
